7328f9fcd02239ee77159659a0ebb0cae17753d7
githubx <akashsharkar786@gmail.com>
2023-11-25 08:21:47 -0800

    Initial Commit

---
+++ githubx/githubxApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubxApp.swift
+//  githubx
+//
+//  Created by Md Akash on 25/11/23.
+//
+
+import SwiftUI
+
+@main
+struct githubxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
